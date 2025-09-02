27e5941c07d7fd5d9c95b2f89c2c0514aeba2640
RifatSarker <rifatsarkercse@gmail.com>
2025-09-02 23:00:39 +0600

    product card

--- myproject/my_app/urls.py
+++ myproject/my_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('products/', views.Products, name='products'),
+    path('home/', views.home, name='home'),
+    path('', views.Products, name='products'),
     path("signin/",views.signin,name="signin"),
     path('signup/', views.Signup, name='signup'),
     path('about/', views.about, name='about'),
